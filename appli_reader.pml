6d8db9ace8eebe45e0f2ba171b470c56020f7d92
s-chiwa <s_chiwaki@mail.goo.ne.jp>
2015-12-31 23:35:33 +0900

    copy file

---
+++ appli_reader.pml
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="appli_reader" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="base" src="base/base.dlg" />
+    </Dialogs>
+    <Resources>
+        <File name="cont_1" src="html/cont_1.html" />
+        <File name="cont_2" src="html/cont_2.html" />
+        <File name="help" src="html/help.html" />
+        <File name="index" src="html/index.html" />
+        <File name="navigation" src="html/navigation.html" />
+        <File name="pepper_help_linereader" src="html/pepper_help_linereader.js" />
+        <File name="pepper_qi" src="html/pepper_qi.css" />
+        <File name="pepper_qi" src="html/pepper_qi.js" />
+        <File name="start" src="html/start.html" />
+        <File name="table" src="html/table.css" />
+    </Resources>
+    <Topics>
+        <Topic name="base_jpj" src="base/base_jpj.top" topicName="base" language="ja_JP" />
+    </Topics>
+    <IgnoredPaths>
+        <Path src=".metadata" />
+    </IgnoredPaths>
+</Package>
---
+++ html/pepper_help_linereader.js
@@ -0,0 +1,71 @@
+﻿var lineMapManual = {};
+
+function lineReader(obj) {
+	sendCommands = "";
+	id = obj.id;
+	//alert(id);
+	data = id.split(parent.spliterKey);
+
+	action = "";
+	operateId = "";
+	dataType = "";
+	goMthod = "";
+	sendValue = "";
+	
+	switch(data.length){
+		case 3:
+			action = data[0];
+			operateId = data[1];
+			dataType = data[2];
+			break;
+		case 4:
+			action = data[0];
+			operateId = data[1];
+			dataType = data[2];
+			goMethod = data[3];
+			break;
+		default:
+			alert("『"+spliterKey+"』でつなぐべきデータの要素が多すぎるか少なすぎます。:::" + id);
+			failureData = true;
+			break;
+	}
+	
+	//alert(action);
+	//alert(operateId);
+	//alert(dataType);
+	
+	operateObj = document.getElementById(operateId);
+	
+	if(dataType.indexOf("TextElement") == 0){
+		sendValue = operateObj.innerText;
+		if(action.indexOf("SayLine") != -1){
+			splitChar = sendValue.substring(0,1);
+			splitCont = sendValue.split(splitChar);
+			if(operateId in lineMapManual){
+				l = lineMapManual[operateId]; 
+				l = l+1;
+				
+				if(l>splitCont.length-1){
+					l=1;
+				}
+				lineMapManual[operateId] = l;
+			}
+			else{
+				lineMapManual[operateId] = 1;
+			}
+			sendValue = splitCont[lineMapManual[operateId]];
+			
+			action = "sendPepperSay";
+		}
+	}
+
+	//alert(action);
+	//alert(operateId);
+	//alert(dataType);
+
+	sendCommands = sendCommands + '["' + action  + '","'  + operateId +  '","' + sendValue + '"]';
+
+	//alert(sendCommands);
+
+	parent.sendPepperFromTabletByCommand(sendCommands);	
+}
